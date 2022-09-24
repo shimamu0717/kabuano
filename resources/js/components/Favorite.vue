abd4e6aa8708db2049bb9903665323bc518514c2
yuta <yuta5229@yahoo.co.jp>
2022-09-24 16:57:30 +0900

    watch-poll前

---
+++ resources/js/components/Favorite.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button
+      type="button"
+      class="btn m-0 p-1 shadow-none">
+      <i class="fas fa-heart mr-1"
+      />
+    </button>
+  </div>
+</template>
+
+<script>
+  
+</script>
